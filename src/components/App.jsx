79150dbdbd45f7a4681b446876416b7955bf4356
Typical-Joey <jtcapocci@yahoo.com>
2021-12-01 10:42:32 -0600

    Move getPokemon code to Pokemon.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,7 @@
-import { useState } from "react";
-import axios from "axios";
 import SideBar from "./SideBar";
 import MainScreen from "./MainScreen";
 
 function App() {
-
-  const [pokemon, setPokemon] = useState('');
-  function getPokemon(){
-    const url = '/getPokemon'; // Backend endpoint
-    axios.get(url)
-    .then(response => {
-      const data = JSON.stringify(response.data);
-      setPokemon(data);
-    })
-    .catch(error => {
-      setPokemon(error);
-    });
-  }
-
   return (
     <>
       <SideBar />
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Pokemon() {
+  const [pokemon, setPokemon] = useState('');
+  function getPokemon(){
+    const url = '/getPokemon'; // Backend endpoint
+    axios.get(url)
+    .then(response => {
+      const data = JSON.stringify(response.data);
+      setPokemon(data);
+    })
+    .catch(error => {
+      setPokemon(error);
+    });
+  }
   return (
     <div>
       
